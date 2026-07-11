aa2a544ed05a513e3fe3a68c2adb5d55891db99e
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-11 07:53:30 +0500

    iters

---
+++ iter1.py
@@ -0,0 +1,10 @@
+numbers = [10 , 20 , 30 , 40 , 50 , 60 , 70 , 80]
+it = iter(numbers) #creating an iter
+
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
