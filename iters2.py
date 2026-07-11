7cf096dfd8732c8d2563f2c83e4bf0f3730a8ec3
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-11 08:00:25 +0500

    iterations

---
+++ iters2.py
@@ -0,0 +1,12 @@
+string = "wahid ali khan" #creating a string
+
+it = iter(string)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
