04a0522246cbd1ce11ab69074424a1c65be52b72
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-15 04:33:13 +0500

    using module

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# function of addtion
+def add(a, b):
+    return a + b
+
+# function of subtraction
+def subtract(a, b):
+    return a - b
+
+# function of multiplication
+def multiply(a, b):
+    return a * b
+
+# function of division
+def divide(a, b):
+    return a / b
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+import calculator
+
+#enter the numbers
+number1 = int(input("Enter the number: "))
+number2 = int(input("Enter the number: "))
+
+#displying the functions
+calculator.add(number1 , number2)
+calculator.subtract(number1 , number2)
+calculator.multiply(number1 , number2)
+calculator.divide(number1 , number2)
+
+#displaying the result
+print(calculator)
