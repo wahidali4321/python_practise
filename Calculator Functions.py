4dcc8b32c44a1588cfb8458203f0b35c5b3a7e0b
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-20 18:43:30 +0500

    calculator function

---
+++ Calculator Functions.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+addition = add(11 , 11)
+subtraction = subtract( 11 , 11)
+multiplication = multiply( 11 , 11)
+division = divide( 11 , 11)
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
