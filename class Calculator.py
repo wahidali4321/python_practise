a3acdecc1401d019cce7debf600eeede22a5f869
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-21 02:16:37 +0500

    calculator

---
+++ class Calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def add(a, b):
+        return a + b
+    def subtract(a, b):
+        return a - b
+    
+    def multiply(a, b):
+        return a * b
+    
+    def divide(a, b):
+        return a / b
+    
+p1 = Calculator()
+p1.add(11 , 11)
+p1.subtract(11 , 11)
+p1.multiply(11 , 11)
+p1.divide(11 , 11)
