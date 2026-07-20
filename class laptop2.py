34db94420892fb1a93b38686475c353c65413052
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-21 01:52:05 +0500

    laptop

---
+++ class laptop2.py
@@ -0,0 +1,19 @@
+class Laptop:
+    brand = "HP"
+    ram = "16 GB"
+    storage = "512 GB SSD"
+
+laptop1 = Laptop()
+laptop2 = Laptop()
+
+print("Laptop 1")
+print(laptop1.brand)
+print(laptop1.ram)
+print(laptop1.storage)
+
+print()
+
+print("Laptop 2")
+print(laptop2.brand)
+print(laptop2.ram)
+print(laptop2.storage)
