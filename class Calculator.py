7363eb6392b83a5b9267f9b7b37bc757b4ec0045
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-21 02:19:22 +0500

    class calculator

--- class Calculator.py
+++ class Calculator.py
@@ -1,20 +1,23 @@
 class Calculator:
-    def add(a, b):
+    def add(self, a, b):
         return a + b
-    def subtract(a, b):
+
+    def subtract(self, a, b):
         return a - b
-    
-    def multiply(a, b):
+
+    def multiply(self, a, b):
         return a * b
-    
-    def divide(a, b):
+
+    def divide(self, a, b):
         return a / b
-    
+
+
 p1 = Calculator()
 p2 = Calculator()
 p3 = Calculator()
 p4 = Calculator()
-p1.add(11 , 11)
-p2.subtract(11 , 11)
-p3.multiply(11 , 11)
-p4.divide(11 , 11)
+
+print(p1.add(11, 11))
+print(p2.subtract(11, 11))
+print(p3.multiply(11, 11))
+print(p4.divide(11, 11))
