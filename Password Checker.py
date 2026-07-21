c7281d0307cbcd7a293cda88f9a2c80e74704434
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-22 02:06:18 +0500

    password checker

--- Password Checker.py
+++ Password Checker.py
@@ -1,6 +1,8 @@
 password = "python123"
-Data = str(input("enter the password"))
+Data = str(input("enter the password : "))
 while Data == password:
     print("Authenicated")
+    break
 else:
     print("enter your password again : ")
+    
