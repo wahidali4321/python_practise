3f20f427594ec371e38b872c7df5fded9d184dea
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-22 23:19:57 +0500

    __init_function

---
+++ __init__function4.py
@@ -0,0 +1,10 @@
+class Person:
+  def __init__(self, name, age=18):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil")
+p2 = Person("Tobias", 25)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
