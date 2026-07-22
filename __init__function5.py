3bd397d214e866c2507c32e580bff538171a1e41
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-22 23:58:09 +0500

    __init__

--- __init__function5.py
+++ __init__function5.py
@@ -4,11 +4,13 @@ class Student:
         self.rollNo = rollNo
         self.department = department
         
-
+print("Student 1")
 p1 = Student("wahid ali " , "22pwdsc0054" , "Computer science and datascience")
 print(p1.name)
 print(p1.rollNo)
 print(p1.department)
+
+print("Student 2")
 
 p2 = Student("ANSHA KHANAM" , "22pwdsc0054" , "computer science")
 print(p2.name)
