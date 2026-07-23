5fdd84c0955b08f5cbc828ea11c5cca340de1015
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-23 14:55:40 +0500

    self.py

---
+++ self.py
@@ -0,0 +1,10 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil", 25)
+p1.greet()
