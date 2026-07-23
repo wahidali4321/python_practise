fe155ec69b8e32d897261044b973831d85740e23
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-23 15:35:51 +0500

    functions4

---
+++ functions4.py
@@ -0,0 +1,4 @@
+def employee(name , salary , city):
+    print("Name : " , name , "\n" ,    "salary :  " , salary , "\n "   , "city :  " ,  city)
+
+employee(name= " wahid ali " , salary= 10000 , city= "Islamabad")
