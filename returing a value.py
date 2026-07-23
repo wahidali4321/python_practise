4002b79a1d713ded2024cc2ebe7a5532b502423f
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-23 16:26:42 +0500

    returing a value

---
+++ returing a value.py
@@ -0,0 +1,8 @@
+def my_function():
+    return ["wahid ali " , "ansha khanam " , "hammad khan " , "akhtar ali khan"]
+
+fruits = my_function()
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
