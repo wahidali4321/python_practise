354c1d51376cd291edb7021367d67ba1fb7a2fdf
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-23 15:06:40 +0500

    self_method

---
+++ self_method0.py
@@ -0,0 +1,12 @@
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def printname(self):
+    print(self.name)
+
+p1 = Person("Tobias")
+p2 = Person("Linus")
+
+p1.printname()
+p2.printname()
