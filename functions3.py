f9ede06ab5a8d8ee4e560390ffb59a1a0980cd8d
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-23 15:29:14 +0500

    functions3

--- functions3.py
+++ functions3.py
@@ -1,4 +1,4 @@
 def student(name , age , department):
-    print(name , '/n' , age , '/n' , department , '/n')
+    print(name , '\n' , age , '\n' , department , '\n')
 
 student(name= "wahid ali" , age= 22 , department= "Computer science")
