87206e0118e89e8f3b5756a331ec13d21a36fccd
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-24 17:37:01 +0500

    Student Details

---
+++ Student Details.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self , name , age , department):
+        self.name = name
+        self.age = age
+        self.department = department
+
+p1 = student("wahid ali " , 22 , "computer scinece")
+print(p1.name)
+print(p1.age)
+print(p1.department)
