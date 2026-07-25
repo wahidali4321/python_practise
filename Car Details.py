8cd8b7b4864898b88ac725d6d670098dfdacdfa6
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-25 13:06:42 +0500

    car details

--- Car Details.py
+++ Car Details.py
@@ -8,4 +8,8 @@ class Car:
         print(self.brand , "\n" , self.model , "\n" , self.year)
 
 x = Car("BMW " , "XC23" , 2022)
+y = Car("MERCEDES" , "X232" , 2055)
+Z = Car("HONDA " , "CIVIC" , 2022)
+y.car_info()
 x.car_info()
+Z.car_info()
