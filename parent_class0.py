2b4202a5b25431867e1137a584b770a71322d196
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-25 12:45:22 +0500

    parent_class

--- parent_class0.py
+++ parent_class0.py
@@ -2,6 +2,12 @@ class Person:
     def __init__(self , fname , lname):
         self.firstName = fname
         self.lastName = lname
-        
+
+    def greet(self):
+        print(self.firstName , self.lastName)
+
+x = Person("WAHID " , "ALI")
+x.greet()
+
 
         
