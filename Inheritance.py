f462336d8cc987598b714260b61a20afdef7c8b6
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-27 19:43:39 +0500

    inheritance

--- Inheritance.py
+++ Inheritance.py
@@ -1,9 +1,11 @@
 lass Person:
-  def __init__(self, fname, lname):
+    def __init__(self, fname, lname):
+    
     self.firstname = fname
     self.lastname = lname
 
-  def printname(self):
+    def printname(self):
+    
     print(self.firstname, self.lastname)
 
 #Use the Person class to create an object, and then execute the printname method:
