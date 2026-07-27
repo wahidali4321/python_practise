3904fecafc03e73154fbd9d40b069d977ec050b6
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-27 20:09:20 +0500

    Cites Tuple.py

---
+++ Cities Tuple.py
@@ -0,0 +1,11 @@
+Cites = cities = (
+    "Peshawar",
+    "Islamabad",
+    "Karachi",
+    "Lahore",
+    "Quetta",
+    "Multan",
+    "Faisalabad",
+    "Hyderabad"
+)
+print(f"First city : {cities(len(0))} , '\n ' , last name : {cities(len(7))}")
