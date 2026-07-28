8a42b677689603ab59181c4e7eba6242d5c28c51
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-07-28 18:44:58 +0500

    Compare the length of two strings

--- Compare the Length of Two Strings.py
+++ Compare the Length of Two Strings.py
@@ -1,10 +1,12 @@
 city1 = "PESHAWAR"
 city2 = "ISLAMABAD"
-print(city1 ,"City1 length : " , len(city1))
-print(city2 , "City2 length : " , len(city2))
 
-if city1 > city2 :
-    print(f"{city1} is lengther ")
+print(city1, "City1 length :", len(city1))
+print(city2, "City2 length :", len(city2))
 
+if len(city1) > len(city2):
+    print(f"{city1} is longer")
+elif len(city1) < len(city2):
+    print(f"{city2} is longer")
 else:
-    print(f"{city2} is lengther")
+    print("Both cities have the same length.")
