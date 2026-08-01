d4d8c92e92c025fd671eaf9cdf604d884fdec28e
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-02 02:00:31 +0500

    employee_salary2.py

---
+++ employee_salary1.py
@@ -0,0 +1,19 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+  def set_age(self, age):
+    if age > 0:
+      self.__age = age
+    else:
+      print("Age must be positive")
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
