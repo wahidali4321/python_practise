5ba0239f340c363ba6110470b8721b33bc25f73d
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-02 02:01:55 +0500

    employee_salary

---
+++ employee_salary2.py
@@ -0,0 +1,9 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+p1 = Person("Emil", 30)
+
+# This is how Python mangles the name:
+print(p1._Person__age) # Not recommended!
