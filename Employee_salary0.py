114c58d38ec7f62f2e8e087672e182b00f782a73
wahidali_4321 <146173354+wahidali4321@users.noreply.github.com>
2026-08-02 01:57:14 +0500

    Employee_salary0.py

---
+++ Employee_salary0.py
@@ -0,0 +1,10 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
+
+  def get_age(self):
+    return self.__age
+
+p1 = Person("Tobias", 25)
+print(p1.get_age())
